ecf4c7696c9c06d7d7af6ca670558a3e8e63c9a0
mnathai <dr.mnathai@gmail.com>
2026-08-20 07:25:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from google import genai
 # ==========================================
 
 def get_db_connection():
-    """Establishes connection to your SQL Server database cleanly."""
+    """Establishes connection cleanly using Streamlit dynamic secrets variables."""
     conn_str = (
         "Driver={ODBC Driver 17 for SQL Server};"
-        "Server=diabetesselector-db-server.database.windows.net;"
-        "Database=mn_diabetes_tt_gen-db;"
-        "Uid=mnaithai;"
-        "Pwd=Mann@1234;"
+        f"Server={st.secrets['server']};"
+        f"Database={st.secrets['database']};"
+        f"Uid={st.secrets['username']};"
+        f"Pwd={st.secrets['password']};"
     )
     return pyodbc.connect(conn_str)
 def map_inputs_to_ids(age, bmi, fbs, duration_text, complication_text):
